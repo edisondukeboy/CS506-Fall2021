fe230ba7d1d887f4356889ffdf93002b99e24098
edisondukeboy <rfshi@bu.edu>
2021-09-23 14:41:36 -0400

    change infrastructure power into a snoop dog

--- 01-python/infrastructure/power.py
+++ 01-python/infrastructure/power.py
@@ -1,3 +1,20 @@
 def draw_power_plant():
-    print("power plant not found")
+    print("Power plant is SnoopDogggggggggg")
+    print('''
+                .---- .
+               .       、
+            _·'__       ·
+        . --($) ($$)---/#\\
+      .`@             /###\\
+      :         ,     #####
+       `-..__.-'  _.- \###/
+           `;_:       `"
+             .'""""""`.
+            /,  hi,    \\\\
+           // hello world\\\\
+          `-._________.-'
+             __`.|.`__
+           (_____|_____)
+''')
+
     return
